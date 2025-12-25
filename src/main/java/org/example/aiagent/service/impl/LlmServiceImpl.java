52226b5dd3dc5679314ff629ffd029ccceb26633
zhemeng.mo <zhemeng.mo@mijahealth.com>
2025-12-25 17:51:04 +0800

    增加停止标记

--- src/main/java/org/example/aiagent/service/impl/LlmServiceImpl.java
+++ src/main/java/org/example/aiagent/service/impl/LlmServiceImpl.java
@@ -8,13 +8,16 @@ import com.openai.models.chat.completions.ChatCompletionChunk;
 import com.openai.models.chat.completions.ChatCompletionCreateParams;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.example.aiagent.dto.LlmResponseDTO;
 import org.example.aiagent.service.LlmService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 
+import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
 @RequiredArgsConstructor
@@ -31,20 +34,25 @@ public class LlmServiceImpl implements LlmService {
         ChatCompletionCreateParams params = ChatCompletionCreateParams.builder()
                 .addUserMessage(prompt)
                 .model(modelName)
+                // 非流式不能开启思考内容
                 .putAdditionalBodyProperty("enable_thinking", JsonValue.from(false))
                 .build();
 
         CompletableFuture<ChatCompletion> future = openAIClient.async().chat().completions().create(params);
         ChatCompletion chatCompletion = future.join();
-        if (!chatCompletion.isValid()) {
-            log.warn("有问题的Completion: {}", chatCompletion);
-        }
 
         LlmResponseDTO llmResponse = new LlmResponseDTO();
-        ChatCompletion.Choice choice = chatCompletion.choices().get(0);
-        if (choice.message().content().isPresent()) {
-            llmResponse.setContent(choice.message().content().get());
+        if (!chatCompletion.isValid()) {
+            log.warn("有问题的回复: {}", chatCompletion);
+            return llmResponse;
         }
+
+        ChatCompletion.Choice choice = chatCompletion.choices().get(0);
+        choice.message().content().ifPresent(content -> {
+            if (StringUtils.isNotBlank(content)) {
+                llmResponse.setContent(content);
+            }
+        });
 
         return llmResponse;
     }
@@ -59,20 +67,38 @@ public class LlmServiceImpl implements LlmService {
         AsyncStreamResponse<ChatCompletionChunk> streamResponse =
                 openAIClient.async().chat().completions().createStreaming(params);
 
+        AtomicBoolean isFinished = new AtomicBoolean(false);
+
         return Flux.create(sink -> {
             streamResponse.subscribe(chunk -> {
-                        if (!chunk.isValid()) {
-                            log.warn("有问题的chunk: {}", chunk);
+                        if (isFinished.get()) {
+                            log.info("流已结束");
+                            return;
                         }
-                        chunk.choices().forEach(choice -> {
+                        if (!chunk.isValid()) {
+                            log.warn("流中出现有问题的chunk: {}， 跳过", chunk);
+                            return;
+                        }
+                        chunk.choices().stream().findFirst().ifPresent(choice -> {
                             LlmResponseDTO llmResponse = new LlmResponseDTO();
-                            if (choice.delta().content().isPresent()) {
-                                llmResponse.setContent(choice.delta().content().get());
+                            choice.delta().content().ifPresent(content -> {
+                                if (StringUtils.isNotBlank(content)) {
+                                    llmResponse.setContent(content);
+                                }
+                            });
+                            Optional.of(choice.delta()._additionalProperties()).ifPresent(additionalProperties -> {
+                                if (additionalProperties.containsKey("reasoning_content") && !additionalProperties.get("reasoning_content").isNull()) {
+                                    llmResponse.setReasoning(additionalProperties.get("reasoning_content").toString());
+                                }
+                            });
+                            if (llmResponse.getContent() != null || llmResponse.getReasoning() != null) {
+                                sink.next(llmResponse);
                             }
-                            if (choice.delta()._additionalProperties().containsKey("reasoning_content")) {
-                                llmResponse.setReasoning(choice.delta()._additionalProperties().get("reasoning_content").toString());
-                            }
-                            sink.next(llmResponse);
+                            choice.finishReason().ifPresent(reason -> {
+                                log.info("流结束，原因: {}", reason);
+                                isFinished.set(true);
+                                sink.complete();
+                            });
                         });
                     }
             );
@@ -83,7 +109,10 @@ public class LlmServiceImpl implements LlmService {
                     sink.complete();
                 }
             });
-            sink.onCancel(streamResponse::close);
+            sink.onCancel(() -> {
+                log.info("前端断开了链接");
+                isFinished.set(true);
+            });
         });
     }
 }
